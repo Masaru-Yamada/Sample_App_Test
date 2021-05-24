c6ba6d2b2118f3412425cbf8752b535accd79a10
Ubuntu <ubuntu@ip-172-31-37-39.ap-northeast-1.compute.internal>
2021-05-24 15:40:11 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   
-  host = 'https://nameless-savannah-84875.herokuapp.com'
+  host = 'nameless-savannah-84875.herokuapp.com'
   #host = '2a81170deb6a49618874ade1e3369264.vfs.cloud9.ap-northeast-1.amazonaws.com'# ここをコピペすると失敗します。自分の環境のホストに変えてください。
   # クラウドIDEの場合は以下をお使いください
   #config.action_mailer.default_url_options = { host: host, protocol: 'https' }
